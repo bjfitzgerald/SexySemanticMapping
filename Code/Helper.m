d33fe5137a3b7e08a0d7e56be9b1d44c6a5ca5bf
bjfitzgerald <bjfitzgerald13@gmail.com>
2016-12-06 02:49:02 -0500

    Obtain Fundamental Matrices

--- Code/Helper.m
+++ Code/Helper.m
@@ -11,7 +11,7 @@ close all
 Path = '../Data/SingleObject/'; 
 SceneNum = 1;
 SceneName = sprintf('%0.3d', SceneNum);
-FrameNum = num2str(1);
+FrameNum = num2str(0);
 
 I = imread([Path,'scene_',SceneName,'/frames/image_',FrameNum,'_rgb.png']);
 ID = imread([Path,'scene_',SceneName,'/frames/image_',FrameNum,'_depth.png']);
@@ -36,6 +36,50 @@ ID = imread([Path,'scene_',SceneName,'/frames/image_',FrameNum,'_depth.png']);
 
 [pcx, pcy, pcz, r, g, b, D_, X, Y,validInd] = depthToCloud_full_RGB(ID, I, './params/calib_xtion.mat');
 Pts = [pcx pcy pcz];
+
+totalFrames = 35;
+frameFeatures = cell(1,totalFrames+1);
+frameValidPoints = cell(1,totalFrames+1);
+fundamentalMatrices = cell(1,totalFrames);
+
+% Make initial assignments at first index, the 0th image
+corners = detectHarrisFeatures(rgb2gray(I));
+[features, valid_corners] = extractFeatures(rgb2gray(I), corners);
+frameFeatures{1,1} = features;
+frameValidPoints{1,1} = valid_corners;
+
+%% Loop to fill cell with x,y,z coords from point cloud
+
+for i=1:totalFrames
+    currentFrameNum = num2str(i);
+    
+    I = imread([Path,'scene_',SceneName,'/frames/image_',FrameNum,'_rgb.png']);
+    ID = imread([Path,'scene_',SceneName,'/frames/image_',FrameNum,'_depth.png']);
+    
+    % Currently not using point cloud coords, which should change
+    [pcx, pcy, pcz, r, g, b, D_, X, Y,validInd] = depthToCloud_full_RGB(ID, I, './params/calib_xtion.mat');
+    Pts = [pcx pcy pcz];
+    
+    % Extract feature neighborhoods
+    corners = detectHarrisFeatures(rgb2gray(I));
+    [features, valid_corners] = extractFeatures(rgb2gray(I), corners);
+    frameFeatures{1,i+1} = features;
+    frameValidPoints{1,i+1} = valid_corners;
+    
+    valid_points1 = frameValidPoints{1,i};
+    valid_points2 = frameValidPoints{1,i+1};
+    
+    % Match the features
+    indexPairs = matchFeatures(frameFeatures{1,i},frameFeatures{1,i+1});
+    
+    matchedPoints1 = valid_points1(indexPairs(:,1),:);
+    matchedPoints2 = valid_points2(indexPairs(:,2),:);
+    
+    % Compute fundamental matrix with RANSAC
+    fundamentalMatrices{1,i} = estimateFundamentalMatrix(matchedPoints1,...
+        matchedPoints2,'Method','RANSAC',...
+        'NumTrials',2000,'DistanceThreshold',1e-4);
+end
 
 %% Display Images and 3D Points
 % Note this needs the computer vision toolbox: you'll have to run this on
