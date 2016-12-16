638eec077464e8ea90ec9e3a29b7363fce0ef715
Gudjon Magnusson <gmagnusson@fc-md.umd.edu>
2016-12-16 11:19:12 -0500

    Tweeked segmentation

--- Code/test/testPcTrim.m
+++ Code/test/testPcTrim.m
@@ -1,6 +1,6 @@
 addpath('../');
-PC = getPointCloud(6, 300);
-PC = pcNormalize(PC);
+PC = getPointCloud(0, 350);
+%PC = pcNormalize(PC);
 
 figure,
 pcshow(PC.Points,PC.Colors);
@@ -9,7 +9,7 @@ title('Full Sample');
 
 fprintf('Trim ');
 tic;
-PCtrim = pcTrim(PC, 0.2);
+PCtrim = pcTrim(PC, 250);
 toc;
 
 figure,
